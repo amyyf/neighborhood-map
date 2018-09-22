da057c871b7c4960ee9e71f8e46d5756a4ae1a37
Amy Frieson <ava447@gmail.com>
2018-09-22 18:24:45 -0400

    docs, style: Update README, lint files

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,3 +1,5 @@
+/* global fetch */
+
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
@@ -18,7 +20,7 @@ const isLocalhost = Boolean(
     )
 );
 
-export default function register() {
+export default function register () {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
@@ -52,7 +54,7 @@ export default function register() {
   }
 }
 
-function registerValidSW(swUrl) {
+function registerValidSW (swUrl) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
@@ -81,7 +83,7 @@ function registerValidSW(swUrl) {
     });
 }
 
-function checkValidServiceWorker(swUrl) {
+function checkValidServiceWorker (swUrl) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
     .then(response => {
@@ -108,7 +110,7 @@ function checkValidServiceWorker(swUrl) {
     });
 }
 
-export function unregister() {
+export function unregister () {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
       registration.unregister();
