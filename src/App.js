2547fc713f117c0cd1c593f518e7fc7314761196
Amy Frieson <ava447@gmail.com>
2018-09-22 18:40:56 -0400

    feat: Create and render List and MapContainer files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import List from './List.js';
+import MapContainer from './MapContainer.js';
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
-          <h1 className='App-title'>Welcome to React</h1>
+          <h1 className='App-title'>Ten of NYC's Oldest Bars</h1>
         </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <List />
+          <MapContainer />
+        </main>
       </div>
     );
   }
---
+++ src/MapContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MapContainer extends Component {
+  render () {
+    return (
+      <div>
+        <p>This will be my map container.</p>
+      </div>
+    );
+  }
+}
+
+export default MapContainer;
