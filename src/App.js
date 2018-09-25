dbeaf54afe10cb0c4e7801f2487ae50f917adae2
Amy Frieson <ava447@gmail.com>
2018-09-24 20:09:08 -0400

    feat: Put markers on map

--- src/App.js
+++ src/App.js
@@ -11,62 +11,82 @@ class App extends Component {
         {
           name: 'Fraunces Tavern',
           id: '507f06e9e4b0d2368fcbada4',
-          lat: '40.70352594119276',
-          lng: '-74.01139528322373'
+          position: {
+            lat: 40.70352594119276,
+            lng: -74.01139528322373
+          }
         },
         {
           name: 'Ear Inn',
           id: '3fd66200f964a52071e61ee3',
-          lat: '40.726083607871615',
-          lng: '-74.00948307272056'
+          position: {
+            lat: 40.726083607871615,
+            lng: -74.00948307272056
+          }
         },
         {
           name: 'McSorley\'s Old Ale House',
           id: '3fd66200f964a52058e41ee3',
-          lat: '40.728602348741525',
-          lng: '-73.98996152288103'
+          position: {
+            lat: 40.728602348741525,
+            lng: -73.98996152288103
+          }
         },
         {
           name: 'Julius\'',
           id: '3fd66200f964a520efe61ee3',
-          lat: '40.73449699128521',
-          lng: '-74.00147997860874'
+          position: {
+            lat: 40.73449699128521,
+            lng: -74.00147997860874
+          }
         },
         {
           name: 'White Horse Tavern',
           id: '3fd66200f964a52039eb1ee3',
-          lat: '40.73568541965826',
-          lng: '-74.00594379881144'
+          position: {
+            lat: 40.73568541965826,
+            lng: -74.00594379881144
+          }
         },
         {
           name: 'Old Town Bar',
           id: '3fd66200f964a52067e91ee3',
-          lat: '40.737537',
-          lng: '-73.989302'
+          position: {
+            lat: 40.737537,
+            lng: -73.989302
+          }
         },
         {
           name: 'P.J. Clarke\'s',
           id: '3fd66200f964a520fbe71ee3',
-          lat: '40.758937738691245',
-          lng: '-73.96846225850372'
+          position: {
+            lat: 40.758937738691245,
+            lng: -73.96846225850372
+          }
         },
         {
           name: 'Bemelmans Bar',
           id: '3fd66200f964a520cdea1ee3',
-          lat: '40.77460663060093',
-          lng: '-73.96330814709152'
+          position: {
+            lat: 40.77460663060093,
+            lng: -73.96330814709152
+          }
         },
         {
           name: 'Landmark Tavern',
           id: '3fd66200f964a5208be81ee3',
-          lat: '40.763350703245884',
-          lng: '-73.99647202623504'
+          position: {
+            lat: 40.763350703245884,
+            lng: -73.99647202623504
+          }
         },
         {
           name: 'Pete\'s Tavern',
           id: '3fd66200f964a52068e31ee3',
-          lat: '40.73645856380347',
-          lng: '-73.98689116651353'
+          position: {
+            lat: 40.73645856380347,
+            lng: -73.98689116651353
+          }
         }
       ]
     };
@@ -80,7 +100,7 @@ class App extends Component {
         </header>
         <main>
           <List places={this.state.places} />
-          <MapContainer />
+          <MapContainer places={this.state.places} />
         </main>
       </div>
     );
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -14,6 +14,24 @@ class MapContainer extends Component {
       center: {lat: 40.7413549, lng: -73.9980244},
       zoom: 13
     });
+    this.renderMarkers(map);
+  }
+
+  renderMarkers (map) {
+    const { places } = this.props;
+    const markers = [];
+    for (let i = 0; i < places.length; i++) {
+      const position = places[i].position;
+      const title = places[i].name;
+      const marker = new google.maps.Marker({
+        position: position,
+        title: title,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        id: i
+      });
+      markers.push(marker);
+    }
   }
 
   render () {
