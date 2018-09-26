8931c5aa1b8d156351ff8c16e72ef8ad5f5f8915
Amy Frieson <ava447@gmail.com>
2018-09-26 11:00:50 -0400

    feat: Open InfoWindow on marker click

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,16 +1,16 @@
 /* global google */
 
 import React, { Component } from 'react';
-import InfoWindow from './InfoWindow.js';
 
 class MapContainer extends Component {
   constructor (props) {
     super(props);
     this.mapDiv = React.createRef();
-    this.infoWindow = React.createRef();
+    this.infoWindow = new google.maps.InfoWindow();
+    this.map = null;
+    this.markers = [];
     this.state = {
-      map: {},
-      markers: []
+      activeMarker: null
     };
   }
 
@@ -19,50 +19,49 @@ class MapContainer extends Component {
     this.renderMap(mapDiv);
   }
 
-  // populateInfoWindow (event) {
-  //   const infowindow = new google.maps.InfoWindow();
-  //   const marker = event.target;
-  //   const map = this.mapDiv.current;
-  //   const window = this.infoWindow.current;
-  //   console.log(marker);
-  //   infowindow.setContent('open');
-  //   infowindow.open(map, marker);
-  // }
+  populateInfoWindow () {
+    this.infoWindow.open(this.map, this.state.activeMarker);
+    this.infoWindow.setContent(this.state.activeMarker.title);
+  }
 
   renderMap (mapDiv) {
     const map = new google.maps.Map(mapDiv, {
       center: {lat: 40.7413549, lng: -73.9980244},
       zoom: 12
     });
-    this.setState({ map: map });
+    this.map = map;
     this.renderMarkers(map);
   }
 
   renderMarkers (map) {
     const { places } = this.props;
-    let markers = [];
     for (let i = 0; i < places.length; i++) {
       const position = places[i].position;
       const title = places[i].name;
+      const id = places[i].id;
       const marker = new google.maps.Marker({
         position: position,
         title: title,
         map: map,
         animation: google.maps.Animation.DROP,
-        id: i
+        id: id
       });
-      markers.push(marker);
+      this.markers.push(marker);
     }
-    this.setState({ markers: markers });
+  }
+
+  setActiveMarker (event) {
+    const markerName = event.target.title;
+    for (let i = 0; i < this.markers.length; i++) {
+      if (this.markers[i].title === markerName) {
+        this.setState({ activeMarker: this.markers[i] }, this.populateInfoWindow.bind(this));
+      }
+    }
   }
 
   render () {
-    /* TODO infowindow - listen for event bubbling on container, target element (which is the marker) and populate infowindow */
-
     return (
-      <div ref={this.mapDiv} onClick={this.populateInfoWindow} style={{width: 500, height: 500}}>
-        <InfoWindow ref={this.infoWindow} />
-      </div>
+      <div ref={this.mapDiv} onClick={(e) => this.setActiveMarker(e)} style={{width: 500, height: 500}} />
     );
   }
 }
